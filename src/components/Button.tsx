ba6fa637974d8ceccff57c92ebfe489b9b9f986e
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-06-17 23:57:28 +0700

    feat : add-assignment -filePicker

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+const Button = ({ isPrimary, text }: { isPrimary: boolean; text: string }) => {
+  return isPrimary ? (
+    <button
+      type="submit"
+      className="bg-secondary-400 rounded-lg font-sen font-bold text-white text-[8px] h-6 w-16 hover:drop-shadow-[0_4px_6px_rgba(188,83,23,0.75)] active:bg-[#BC5317] lg:text-[16px] lg:h-12 lg:w-36"
+    >
+      {text}
+    </button>
+  ) : (
+    <button className="border-[3px] border-secondary-400 rounded-lg bg-white font-sen font-bold text-secondary-400 text-[8px] h-6 w-16 hover:drop-shadow-[0_4px_6px_rgba(188,83,23,0.75)] active:text-white active:bg-secondary-400 lg:text-[16px] lg:h-12 lg:w-36">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
