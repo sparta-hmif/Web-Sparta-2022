2d77be470fd99c77ddeff5f8918ec2a74f74fea2
Rava Maulana <ravamaulana14@gmail.com>
2023-06-18 23:16:12 +0700

    docs: added endpoint documentation

--- src/app/api/score/route.ts
+++ src/app/api/score/route.ts
@@ -3,28 +3,43 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-type postBody = {
-  nim: string;
-};
-
+/**
+ * Endpoint: api/score
+ * Method: GET
+ *
+ * Returns list of spartans sorted by score in descending order
+ */
 export async function GET() {
-  const spartans = await prisma.user.findMany({
-    select: {
-      nim: true,
-      fullName: true,
-      score: true,
-    },
-    where: {
-      role: "PESERTA",
-    },
-    orderBy: {
-      score: "desc",
-    },
-  });
+  try {
+    const spartans = await prisma.user.findMany({
+      select: {
+        nim: true,
+        fullName: true,
+        score: true,
+      },
+      where: {
+        role: "PESERTA",
+      },
+      orderBy: {
+        score: "desc",
+      },
+    });
 
-  return NextResponse.json({ spartans });
+    return NextResponse.json({ spartans });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
 
+/**
+ * Endpoint: api/score
+ * Method: POST
+ *
+ * Updates the score of a spartan
+ */
 export async function POST(req: NextRequest) {
   try {
     const { nim, score } = await req.json();
