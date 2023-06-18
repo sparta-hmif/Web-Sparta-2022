bc3b459503b38fe21c8df20f2c4ffe7b4a848a6f
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-06-18 23:18:31 +0700

    Feat:Checkbox Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import Checkbox from "@/components/Checkbox";
 export default function Home() {
-  return <h1 className="text-green-500 text-7xl">HELLO</h1>;
+  return (
+    <div className="bg-black">
+      <Checkbox />
+    </div>
+  );
 }
---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Checkbox = () => {
+  return (
+    <input type="checkbox" className='w-4 h-4 border-2 border-[#661E29] text-[#661E29] rounded hover:bg-red-600' />
+  )
+}
+
+export default Checkbox
