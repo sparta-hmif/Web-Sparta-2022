4d1561a3a6cfa9f96f9d045927ab46694d5988b3
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-06-19 23:28:05 +0700

    refactor: eval day function and get

--- src/app/api/day/route.ts
+++ src/app/api/day/route.ts
@@ -10,11 +10,12 @@ const prisma = new PrismaClient();
  * Data day yang baru ditambahin disimpen ke database
  * [Sementara] Buat endpoint ini gaperlu validasi role, tapi nanti perlu ditambahin validasi
  */
-
-async function POSTDay(req: NextRequest) {
+// aman
+async function POST(req: NextRequest) {
   try {
     const { number, date, description } = await req.json();
 
+    // number hrs bs 0
     if (!(number && date)) {
       return NextResponse.json(
         { message: "Request body must at least contain 'number' and 'date'" },
@@ -44,9 +45,10 @@ async function POSTDay(req: NextRequest) {
  *
  * Endpoint dipake di page day buat nampilin semua day ke peserta + nampilin evaluasi day yang udah pernah dibuat sama peserta
  */
-async function GETDays(req: NextRequest) {
+async function GET(req: Request) {
   try {
-    const { userId } = await req.json();
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
     const days = await prisma.day.findMany({
       orderBy: {
         number: "asc",
@@ -55,55 +57,22 @@ async function GETDays(req: NextRequest) {
       include: {
         EvalDay: {
           where: {
-            userId: userId,
+            userId: String(userId),
           },
         },
       },
     });
 
-    return NextResponse.json({ days });
-  } catch (err) {
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
-
-/**
- * Endpoint Penambahan Evaluasi Day
- * Endpoint dipake di page day buat peserta bisa ngasih evaluasi personal tentang keberlangsungan day
- * Rating diantara 1-5
- */
-async function POSTEval(req: NextRequest) {
-  try {
-    const { rating, evaluation, userId, dayId } = await req.json();
-
-    if (!(userId && dayId && rating && evaluation)) {
-      return NextResponse.json(
-        {
-          message:
-            "Request body must at least contain 'userId', 'dayId', 'rating', and 'evaluation'",
-        },
+    if (!days) {
+      NextResponse.json(
+        { message: "gaada days" },
         { status: 400 }
       );
     }
 
-    if (rating < 1 || rating > 5) {
-      return NextResponse.json({ message: "Invalid rating" }, { status: 400 });
-    }
-
-    await prisma.evalDay.create({
-      data: {
-        rating: rating,
-        evaluation: evaluation,
-        userId: userId,
-        dayId: dayId,
-      },
-    });
-
-    return NextResponse.json({ message: "success" });
-  } catch (err) {
+    return NextResponse.json(days);
+  } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
@@ -111,47 +80,4 @@ async function POSTEval(req: NextRequest) {
   }
 }
 
-/**
- * Endpoint Edit Evaluasi Day
- * Endpoint dipake di page day buat peserta bisa ngasih evaluasi personal tentang keberlangsungan day
- * Rating diantara 1-5
- */
-async function PATCHEval(req: NextRequest) {
-  try {
-    const { id, rating, evaluation } = await req.json();
-
-    if (!(id && (rating || evaluation))) {
-      return NextResponse.json(
-        {
-          message:
-            "Request body must at least contain 'id', and one of these: 'rating' or 'evaluation'",
-        },
-        { status: 400 }
-      );
-    }
-
-    if (rating < 1 || rating > 5) {
-      return NextResponse.json({ message: "Invalid rating" }, { status: 400 });
-    }
-
-    await prisma.evalDay.update({
-      where: {
-        id: id,
-      },
-
-      data: {
-        rating: rating ? rating : undefined,
-        evaluation: evaluation ? evaluation : undefined,
-      },
-    });
-
-    return NextResponse.json({ message: "success" });
-  } catch (err) {
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
-
-export { POSTDay, GETDays, POSTEval, PATCHEval };
+export { GET, POST }
