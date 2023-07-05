f4ceff3c04ae8655d231210db81e6bfa2dc991c2
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-07-05 22:36:51 +0700

    feat: aws s3 configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+          `${process.env.S3_BUCKET_NAME}.s3.amazonaws.com`,
+          `${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com`,
+        ],
+      },
+}
 
 module.exports = nextConfig
--- src/pages/api/s3-upload.js
+++ src/pages/api/s3-upload.js
@@ -1,2 +1,12 @@
 // pages/api/s3-upload.js
-export { APIRoute as default } from "next-s3-upload";
+import { APIRoute, sanitizeKey } from "next-s3-upload";
+
+export default APIRoute.configure({
+  key(req, filename) {
+    return `spartans/${Date.now()}${sanitizeKey(filename)}`;
+  },
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  bucket: process.env.S3_BUCKET_NAME,
+  region: process.env.S3_REGION
+});
