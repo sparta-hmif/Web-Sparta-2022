d2ee8fc0a39d43638fd7c64c299a5ff9e2c9dc27
Rava Maulana <ravamaulana14@gmail.com>
2023-07-08 20:54:12 +0700

    fix: change env

--- src/pages/api/s3-upload.js
+++ src/pages/api/s3-upload.js
@@ -5,8 +5,8 @@ export default APIRoute.configure({
   key(req, filename) {
     return `spartans/${Date.now()}${sanitizeKey(filename)}`;
   },
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.SPARTA_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.SPARTA_AWS_SECRET_KEY,
   bucket: process.env.S3_BUCKET_NAME,
-  region: process.env.S3_REGION
+  region: process.env.S3_REGION,
 });
