9be3df21460e9bdc2b1856bc0b8564952708040e
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-07-10 17:29:51 +0700

    Update ProfilKasuh.tsx

--- src/components/profil-kasuh/ProfilKasuh.tsx
+++ src/components/profil-kasuh/ProfilKasuh.tsx
@@ -1,52 +1,104 @@
-
+"use client";
+import { useState } from "react";
 import Button from "../Button";
-import {AiFillInstagram}  from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 
-const ProfilKasuh = () => {
+interface profilProps{
+  urlPhoto:string;
+  namaIg:string;
+  tglLahir:string;
+  namaLengkap:string;
+  namaPanggilan:string;
+  nim:string;
+  jurusan:string;
+  deskripsi:string;
+}
+
+const ProfilKasuh = ({urlPhoto,namaIg,tglLahir,namaLengkap,namaPanggilan,nim,jurusan,deskripsi} : profilProps) => {
+  const [hasRegistered, setHasRegistered] = useState(false);
   return (
     <>
       <div className="w-full h-[960px] bg-[url('/images/profil-kasuh/background.svg')] bg-cover flex justify-center pt-16 md:pt-20 lg:items-center">
         <div className="w-[96%] md:w-[80%] h-[720px] lg:h-[75%] bg-white rounded-xl border-[#560C15] border-4 pt-10 md:pt-16 lg:px-16">
           <div className="flex flex-col justify-center items-center lg:items-start lg:flex-row gap-4 lg:gap-11 px-4 mb-9">
-            <div className="w-40 h-40 lg:w-80 lg:h-[450px] bg-[#D9D9D9] rounded-xl"></div>
+            <div className="w-40 h-40 lg:w-80 lg:h-[450px] bg-[#D9D9D9] rounded-xl">
+              <img src={urlPhoto}/>
+            </div>
             <div className="w-auto md:w-[520px] h-[400px] md:h-[360px]">
               <div className="flex justify-between mb-1">
                 <div className="flex items-center gap-1">
-                <AiFillInstagram/>
-                  <p className="font-koulen text-black text-[20px]">NAMA_IG</p>
+                  <AiFillInstagram />
+                  <p className="font-koulen text-black text-[20px]">{namaIg}</p>
                 </div>
                 <div className="flex items-center gap-1">
-                  <img src="/images/profil-kasuh/gift.svg"/>
-                  <p className="font-koulen text-black text-[20px]">6/6/2026</p>
+                  <img src="/images/profil-kasuh/gift.svg" />
+                  <p className="font-koulen text-black text-[20px]">{tglLahir}</p>
                 </div>
               </div>
               <h4 className="text-primaryDark-400 font-koulen mb-1">
-                NAMA LENGKAP DARI PENGGUNA
+                {namaLengkap}
               </h4>
               <h6 className="text-secondaryDark-500 font-koulen mb-1">
-                NAMA PANGGILAN: TANGGA
+                NAMA PANGGILAN: {namaPanggilan}
               </h6>
               <h6 className="text-secondaryDark-500 font-koulen mb-1">
-                NIM: 13521000
+                NIM: {nim}
               </h6>
               <h6 className="text-secondaryDark-500 font-koulen mb-1">
-                JURUSAN: STIF
+                JURUSAN: {jurusan}
               </h6>
               <p className="caption md:body-1">
-                Deskripsi tentang kasuh (termasuk hobby, mbti, interset,
-                domisili, dll) Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Praesent velit libero, hendrerit sed odio et,
-                finibus condimentum mi. Praesent tristique, arcu ut efficitur
-                porta, nulla metus maximus mauris, sed finibus urna ex non
-                ligula. Proin quis ante non nisl iaculis tincidunt eget vitae
-                nisl.
+                {deskripsi}
               </p>
             </div>
           </div>
-          <div className="w-full flex justify-center lg:justify-end">
-            <div className="w-36 h-12">
-              <Button isPrimary={true} text={"Daftar"} type={"button"} />{" "}
-            </div>
+          <div className="w-full flex justify-center lg:justify-end gap-4">
+            {!hasRegistered ? (
+              <div className="w-36 h-12">
+                <Button isPrimary={true} text={"Daftar"} type={"button"} />{" "}
+              </div>
+            ) : (
+              <>
+                <div className="w-36 h-12">
+                  <button
+                    type="button"
+                    className="
+        transition
+        w-full 
+        py-3 px-2 font-sen
+        bg-primary-400
+        border-secondary-400
+        rounded-xl md:rounded-2xl
+        font-bold 
+        text-xs md:text-base
+        hover:drop-shadow-[0_3px_6px_rgba(188,83,23,0.55)]       
+        text-white
+        "
+                  >
+                    Edit Pilihan
+                  </button>
+                </div>
+                <div className="w-36 h-12">
+                  <button
+                    type="button"
+                    className="
+        transition
+        w-full 
+        py-3 px-2 font-sen
+        bg-primaryDark-200
+        border-secondary-400
+        rounded-xl md:rounded-2xl
+        font-bold 
+        text-xs md:text-base
+        hover:drop-shadow-[0_3px_6px_rgba(188,83,23,0.55)]       
+        text-white
+        "
+                  >
+                    Hapus Pilihan
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
