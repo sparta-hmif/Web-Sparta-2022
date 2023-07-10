9ca531035dee6bd87704c9ad8819290c7babb79f
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-07-10 17:31:03 +0700

    Update page.tsx

--- src/app/(pages)/(site)/page.tsx
+++ src/app/(pages)/(site)/page.tsx
@@ -2,15 +2,13 @@ import Navbar from "@/components/Navbar";
 import About from "./components/About";
 import BehindTheGreek from "./components/BehindTheGreek";
 import Hero from "./components/Hero";
-import ProfilKasuh from "../../../components/profil-kasuh/ProfilKasuh"
 
 export default function Home() {
   return (
     <div>
-      {/* <Hero />
+      <Hero />
       <About />
-      <BehindTheGreek /> */}
-      <ProfilKasuh/>
-    </div>
+      <BehindTheGreek />
+      </div>
   );
 }
