35281326b26f2f42352eeab0999c16a4e58a46b8
Rava Maulana <ravamaulana14@gmail.com>
2023-07-17 21:22:44 +0700

    Deployment 2.2

--- src/app/(pages)/(require-login)/careerpath/components/CareerPathCountdown.tsx
+++ src/app/(pages)/(require-login)/careerpath/components/CareerPathCountdown.tsx
@@ -2,14 +2,14 @@ import dynamic from "next/dynamic";
 
 const CareerPathCountdown = ({ targetDate }: { targetDate: Date }) => {
   const targetDateTime = targetDate.getTime();
-  const DynamicCountdown = dynamic(
-    () => import("@/components/pemilihan-desuh/components/Coundown"),
-    { ssr: false }
-  );
+  // const DynamicCountdown = dynamic(
+  //   () => import("@/components/pemilihan-desuh/components/Coundown"),
+  //   { ssr: false }
+  // );
   return (
     <>
       <div className="lg:scale-90">
-        <DynamicCountdown target={targetDateTime} pemilihan="Career Path" />
+        {/* <DynamicCountdown target={targetDateTime} pemilihan="Career Path" /> */}
       </div>
     </>
   );
