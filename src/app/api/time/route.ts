9779db52c7fde388437308c5d38ec6ca8932653e
kevinssheva <kevinssheva@gmail.com>
2023-07-28 15:39:24 +0700

    oaijdwiadd

--- src/app/api/time/route.ts
+++ src/app/api/time/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
+export function GET(req: NextRequest) {
+  revalidatePath(req.url);
   return NextResponse.json({ time: new Date().toISOString() });
 }
