36cdc6d42f8fdf989190d5900d966715a04b71f0
Rava Maulana <ravamaulana14@gmail.com>
2023-08-14 00:11:22 +0700

    fix: change date

--- src/app/api/(kasuh)/constants/date.ts
+++ src/app/api/(kasuh)/constants/date.ts
@@ -1,4 +1,4 @@
-export const ACCEPT_START: Date = new Date(2023, 7, 14, 0, 0);
+export const ACCEPT_START: Date = new Date("2023-08-13T17:00:00.000Z");
 
 export const FIRST_PRIO_END: Date = new Date(2023, 7, 27, 23, 59);
 export const SECOND_PRIO_END: Date = new Date(2023, 8, 1, 23, 59);
